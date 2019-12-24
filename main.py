9ea239a4889c9a30506a9294446f45451ca5deb7
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2019-12-24 09:50:02 +0800

    1

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ import os
 import sys
 import cv2
 import PyQt5
-# import matplotlib
-# import matplotlib.pyplot as plt
-from PyQt5 import QtGui, QtWidgets, QtTest
+import numpy as np
+import matplotlib.pyplot as plt
+from PyQt5 import QtWidgets
 from Ui_main import Ui_Form as Ui_main
+
+
+# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_calib3d/py_depthmap/py_depthmap.html#code
+def showDisparity():
+    imgL = cv2.imread('resources/imL.png', 0)
+    imgR = cv2.imread('resources/imR.png', 0)
+
+    stereo = cv2.StereoBM_create(numDisparities=64, blockSize=9)
+    disparity = stereo.compute(imgL, imgR)
+    plt.imshow(disparity, 'gray')
+    plt.show()
+
 
 if __name__ == '__main__':
     os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = os.path.join(
@@ -15,7 +27,7 @@ if __name__ == '__main__':
     ui = Ui_main()
     ui.setupUi(Widget)
 
-    # ui.pushButton_5.clicked.connect(augmentedReality)
+    ui.pushButton.clicked.connect(showDisparity)
 
     Widget.show()
     sys.exit(app.exec_())
